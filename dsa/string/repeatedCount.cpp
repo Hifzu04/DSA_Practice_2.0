75cf699decb0f6dcf4fcb607b6fb99bfa50ca953
Hifzu04 <hifzurbtech@gmail.com>
2024-01-08 18:55:59 +0530

    pqret

--- dsa/string/repeatedCount.cpp
+++ dsa/string/repeatedCount.cpp
@@ -1,78 +1,53 @@
-//  #include <iostream>
-//  #include <string>
-//  #include <vector>
-//  using namespace std;
-// int main()
-// {
-//     vector<string> vec;
-//     vector<string>vec2;
-//     for (int i = 0; i < 10; i++)
-//     {
-//         string input;
-//         cin>>input;
-//         vec.push_back(input);
-//     }
-//     int count = 1;
-    
-//     for (int i = 0; i < 9; i++)
-//     {
-//         if (vec[i] == vec[i + 1])
-//         {
-//             count++;
-            
-//         }
-//         else{
-//             string cnt = to_string(count);
-//             vec2.push_back(vec[i]);
-//             vec2.push_back(cnt);
-//             count=1;
-//         }
 
-//     }
-//     for(int i=0 ; i<10; i++){
-//         cout<<vec2[i]<<" ";
-//     }
-// }
+// Write a function to perform basic string compression using the counts of repeated characters,
+// also known as Run Length Encoding. 
+// Let see one example,
+// the input string "aaaabcccccaaa" would become "a4b1c5a3". 
+// If the "compressed" string would not become smaller than the original string,
+// your function should return the input string.
+// You can assume the string has only uppercase and lowercase letters.
+//  You may use the to_string(int) method to convert an integer into string.
 
+// Sample Inputs
 
+//     bbbaaaadexxxxxx
+//     abc
+
+// Sample Outputs
+
+//     b3a4d1e1x6
+//     abc
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 using namespace std;
-
-int main() {
-    vector<string> vec;
-    vector<string> vec2;
-
-    for (int i = 0; i < 10; i++) {
-        string input;
-        cin >> input;
-        vec.push_back(input);
-    }
-
+int main()
+{
+    string str;
+    cin >> str;
     int count = 1;
-
-    for (int i = 0; i < 9; i++) {
-        if (vec[i] == vec[i + 1]) {
+    bool flag =0;
+    string compressedstring;
+    for (int i = 1; i < 11; ++i)
+    {
+        if (str[i] == str[i - 1])
+        {
             count++;
-        } else {
-            string cnt = to_string(count);
-            vec2.push_back(vec[i]);
-            vec2.push_back(cnt);
+            flag=1;
+
+        }
+        else
+        {
+            compressedstring += str[i - 1] + to_string(count);
             count = 1;
         }
     }
-
-    // Handle the last element separately
-    string cnt = to_string(count);
-    vec2.push_back(vec.back());
-    vec2.push_back(cnt);
-
-    // Output vec2
-    for (int i = 0; i < vec2.size(); i++) {
-        cout << vec2[i] << " ";
+    if(flag==0){
+       cout<<str;
+       return 0;
     }
-
-    return 0;
+    // if(str[9]!=str[8]){
+    //     compressedstring+= str[9]+'1';
+    // }
+    cout << compressedstring;
 }
-            
