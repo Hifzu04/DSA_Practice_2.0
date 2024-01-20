8fa3d99bfb743e2b00ee3f301220ad44bdc0f449
Hifzu04 <hifzurbtech@gmail.com>
2024-01-20 21:20:56 +0530

    largestSubstring

---
+++ Sliding_window/uniqueSubstring.cpp
@@ -0,0 +1,25 @@
+//unique substring (hashing method)
+/*given a string, write a function to print lagrest unique substring
+sample Inputs:
+prateekbhaiya
+aabcb
+
+sample output
+ekbhaiy
+abc */
+
+
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string largest_substring(string str){
+    
+}
+int main(){
+    string str;
+    cin>>str;
+    cout<<largest_substring(str)<<endl;
+}
