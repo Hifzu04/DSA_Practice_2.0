e424d21b2b2b02f92032f39aaa80aec7c04634f2
Hifzu04 <hifzurbtech@gmail.com>
2024-01-29 23:38:18 +0530

    smallest distinct substring

---
+++ Sliding_window/smallesDistinctWindow.cpp
@@ -0,0 +1,21 @@
+/*
+Smallest Distinct Window
+
+Given a string, find the smallest window (substring) 
+which contains all distinct characters of the given input string.
+
+Sample Inputs
+
+    aabcbcdbcaaad
+    aaaa
+
+Sample Outputs
+
+    dbca
+    a
+
+Explanation : The smallest window is "dbca" as it contains all
+ the distinct letters from input string. Similarly for the second case the answer should be 'a'.
+*/
+#include<iostream>
+
