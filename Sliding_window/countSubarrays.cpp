f536f12c9a23d166ad09577615d79950a0796007
Hifzu04 <hifzurbtech@gmail.com>
2024-02-01 23:02:57 +0530

    init

---
+++ Sliding_window/countSubarrays.cpp
@@ -0,0 +1,38 @@
+/*
+Given an unsorted array of integers, find the number of subarrays having sum exactly equal to a given number k.
+
+Sample Input
+
+    arr = {10, 2, -2, -20, 10}
+    K = -10
+
+
+Sample Output
+
+3
+
+
+Explanation
+
+10 + 2 - 2 + -20 = 10
+
+-20 + 10 = -10
+
+2 + -2 + -20 + 10 = -10
+
+Expected Complexity
+Linear
+
+
+Hint : You may require a hashmap.
+All changes saved
+|
+Line 1, Column 1
+Coding Exercise*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    
+}
