6144a82d441f572d42594cfdead9e1fa288e93b1
Hifzu04 <hifzurbtech@gmail.com>
2024-02-01 22:58:03 +0530

    solution to be optimised

--- Sliding_window/smallesDistinctWindow.cpp
+++ Sliding_window/smallesDistinctWindow.cpp
@@ -18,4 +18,28 @@ Explanation : The smallest window is "dbca" as it contains all
  the distinct letters from input string. Similarly for the second case the answer should be 'a'.
 */
 #include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+string Smallestwindow(string str){
+    //storing the elemet in a hasmap
+    unordered_map<char,int>mpp;
+    string uniqueestr;
+    for(char ch: str){
+        mpp[ch]++;
+        if (uniqueestr.find(ch) == -1)
+        {
+         uniqueestr +=ch;
+        }
+        
+    }
+    
+     return uniqueestr;
+     
+}
+int main(){
+    string str;
+    cin>>str;
+    cout<<Smallestwindow(str)<<endl;
+}
 
