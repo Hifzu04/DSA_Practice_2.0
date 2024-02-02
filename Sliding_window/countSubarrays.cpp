8778dbe837bfbad1f1fde082c7ae5fb675de403e
Hifzu04 <hifzurbtech@gmail.com>
2024-02-02 23:13:22 +0530

    to be improved

--- Sliding_window/countSubarrays.cpp
+++ Sliding_window/countSubarrays.cpp
@@ -1,38 +1,45 @@
 /*
 Given an unsorted array of integers, find the number of subarrays having sum exactly equal to a given number k.
-
 Sample Input
-
     arr = {10, 2, -2, -20, 10}
     K = -10
-
-
 Sample Output
-
 3
-
-
 Explanation
-
 10 + 2 - 2 + -20 = 10
-
 -20 + 10 = -10
-
 2 + -2 + -20 + 10 = -10
-
 Expected Complexity
 Linear
-
-
 Hint : You may require a hashmap.
-All changes saved
-|
-Line 1, Column 1
-Coding Exercise*/
+*/
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    
+int main()
+{
+    // brute force. O(n)^2
+    int arr[5] = {10, 2, -2, -20, 10};
+    int k = -10;
+    int count = 0;
+    int sum = 0;
+    int j = 0;
+    for (int i = 0; i < 5; i++)
+    {
+
+        while (sum > k && j<5 )
+        {
+            sum += arr[j];
+            j++;
+            if (sum == k)
+            {
+                count++;
+                j = i;
+                
+            }
+        }
+        sum = 0;
+    }
+    cout << "num of subarray with sum ==k is " << count << endl;
 }
