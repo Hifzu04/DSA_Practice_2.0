74f75c9f3eb028178a95e5100089ddcee0965551
Hifzu04 <hifzurbtech@gmail.com>
2024-03-21 23:47:10 +0530

    merge sort revist

---
+++ recursion/quickSort.cpp
@@ -0,0 +1,15 @@
+#include>iostream>
+usign namespace std;
+
+void quicksort(*arr , s , e){
+    
+}
+
+int main(){
+    int arr[10] = {9,8,3,4,5,6,7,1,2,0};
+    int size = 10;
+    quickSort(arr, 0 , size-1);
+    for(int i = 0 ; i<10  ; i++){
+        cout<<arr[i]<<endl;
+    }cout<<endl;
+}
