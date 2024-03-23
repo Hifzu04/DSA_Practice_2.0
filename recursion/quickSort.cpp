31747a48879bba3316caa210e3902aaf9d052b2d
Hifzu04 <hifzurbtech@gmail.com>
2024-03-23 15:25:42 +0530

    quick sort improvement

--- recursion/quickSort.cpp
+++ recursion/quickSort.cpp
@@ -1,30 +1,67 @@
-#include>iostream>
-usign namespace std;
+#include<iostream>
+using namespace std;
 
-
-int partionAndPivit(int )
-
-void quicksort(int *arr , int s , int e){
-    //base case
-    if(s>=e){
-    return;
+int partitionAndPivot(int arr[], int s, int e)
+{ // doubt : original array won't change??
+    // pivot selection
+    int pivot = arr[s];
+    // placing pivot at its correct positon
+    int count = 0;
+    for (int i = s+1; i <= e; i++)
+    {
+        if(arr[i]<pivot){
+            count++;
+        }
     }
-    //select pivit and arrange all element less than and greater that pivit on lhs and rhs of pivit then  do patrition 
-    int partition  = partitionAndPivit(arr , s , e );
+    int pivotIndex = s+count;
+    swap(arr[pivotIndex] ,pivot);
 
-    //do the same case for lhs  and rhs of the pivited element.
-    quicksort(arr , s , partition-1);
-    quicksort(arr , partition+1 , e);
 
-    
-
+    //lhs and rhs arrangemet wrt pivot
+    //in lhs of pivot do all the element smaller than that
+    //in rhs of pivot do all the element larger than that
+    int i = s;
+    int j= e;
+    while(i<pivotIndex && j>pivotIndex){
+        if(arr[j]<pivot and arr[i]>pivot){
+            swap(arr[i] , arr[j]);
+            i++;
+            j--;
+        }
+        if(arr[i]<pivot and arr[j]<pivot){
+            i++;
+        }
+        if(arr[i]>pivot and arr[j]>pivot){
+            j--;
+        }
+    }
+    //return index of pivit after it gets at postion of partition (correct position)
+    return pivotIndex;
 }
 
-int main(){
-    int arr[10] = {9,8,3,4,5,6,7,1,2,0};
+void quickSort(int arr[], int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+    // select pivot and arrange all element less than and greater that pivot on lhs and rhs of pivot then  do patrition.
+    int partition = partitionAndPivot(arr, s, e);
+
+    // do the same case for lhs  and rhs of the pivoted element.
+    quickSort(arr, s, partition - 1);
+    quickSort(arr, partition + 1, e);
+}
+
+int main()
+{
+    int arr[10] = {9, 8, 3, 4, 5, 6, 7, 1, 2, 0};
     int size = 10;
-    quickSort(arr, 0 , size-1);
-    for(int i = 0 ; i<10  ; i++){
-        cout<<arr[i]<<endl;
-    }cout<<endl;
+    quickSort(arr, 0, size-1);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
