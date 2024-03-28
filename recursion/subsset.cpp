62622221ae66e4838940e1bd20a199f8650f7239
Hifzu04 <hifzurbtech@gmail.com>
2024-03-28 08:17:08 +0530

    progress

--- recursion/subsset.cpp
+++ recursion/subsset.cpp
@@ -1,11 +1,40 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    vector<int>vec;
-    for(int i=0; i<n; i++){
-        int 
+//return all the subset(power set) of an array (num of subset : 2^N)
+//algorithm is most imp
+
+void subSet(&vector<vector<int>>powerSet , vector<int>eachSubset ,  int index){
+
+    //base case 
+    if(index >= 3 ){
+        powerSet.push_back(eachSubset);
+        return;
     }
+
+    //exclude wala case
+    subSet(powerSet , eachSubset , index+1);
+
+    //include wala case
+    int elemet = 
+    subSet(powerSet , eachSubset , index+1);
+
+
+}
+
+
+
+
+int main(){
+    vector<int>set {1,2,3};
+
+    //for whole power set(as final answer); 
+    vector<vector<int>>powerSet;
+    
+    //for each subset
+    vector<int>eachSubset;
+    int index =0;
+    subSet(powerSet, eachSubset, index);
 
 
 }
