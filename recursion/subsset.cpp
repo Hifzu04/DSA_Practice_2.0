eae42c83cd7ba9877668a41926a617beb29e28d5
Hifzu04 <hifzurbtech@gmail.com>
2024-03-28 22:58:02 +0530

    code is running smoothly, yeeaa! i made it using recursion

--- recursion/subsset.cpp
+++ recursion/subsset.cpp
@@ -1,40 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-//return all the subset(power set) of an array (num of subset : 2^N)
-//algorithm is most imp
+// return all the subset(power set) of an array (num of subset : 2^N)
+// algorithm is most imp
 
-void subSet(&vector<vector<int>>powerSet , vector<int>eachSubset ,  int index){
+void subSet(vector<int> set, vector<vector<int>> &powerSet, vector<int> eachSubset, int index)
+{
 
-    //base case 
-    if(index >= 3 ){
+    // base case
+    if (index >= set.size())
+    {
         powerSet.push_back(eachSubset);
         return;
     }
 
-    //exclude wala case
-    subSet(powerSet , eachSubset , index+1);
+    // exclude wala case
+    subSet(set, powerSet, eachSubset, index + 1);
 
-    //include wala case
-    int elemet = 
-    subSet(powerSet , eachSubset , index+1);
-
-
+    // include wala case
+    int elemet = set[index];
+    eachSubset.push_back(elemet);
+    subSet(set, powerSet, eachSubset, index + 1);
 }
 
+int main()
+{
+    vector<int> set{1, 2, 3,4};
 
+    // for whole power set(as final answer);
+    vector<vector<int>> powerSet;
 
+    // for each subset
+    vector<int> eachSubset;
+    int index = 0;
+    subSet(set, powerSet, eachSubset, index);
 
-int main(){
-    vector<int>set {1,2,3};
-
-    //for whole power set(as final answer); 
-    vector<vector<int>>powerSet;
-    
-    //for each subset
-    vector<int>eachSubset;
-    int index =0;
-    subSet(powerSet, eachSubset, index);
-
-
+    // it is vector itself
+    for (auto vctr : powerSet)
+    {
+        for (auto it : vctr)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
 }
