1a34d25865577cf525a427cb29d323a3381d86f1
Hifzu04 <hifzurbtech@gmail.com>
2024-04-02 00:13:24 +0530

    done

--- recursion/phoneKeypad.cpp
+++ recursion/phoneKeypad.cpp
@@ -1,46 +1,43 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void keypad(int index, string number , string* mapping , vector<string>& finalAnswer, string individualAnswer){
-    //base case 
-    if(index>=number.length()){
+void keypad(int index, string number, string *mapping, vector<string> &finalAnswer, string individualAnswer)
+{
+    // base case
+    if (index >= number.length())
+    {
         finalAnswer.push_back(individualAnswer);
         return;
     }
-     
-    //convert each digit of string number into int number
+
+    // convert each digit of string number into int number
     int num = number[index] - '0';
-    //takeout correcponding value to number. ie for 2 => abc
+    // takeout correcponding value to number. ie for 2 => abc, for 3 =>  def
     string value = mapping[num];
 
-    for(int i=0; i<value.length() ; i++){
+    for (int i = 0; i < value.length(); i++)
+    {
         individualAnswer.push_back(value[i]);
-        keypad(index+1, number ,  mapping , finalAnswer,individualAnswer);
+        keypad(index + 1, number, mapping, finalAnswer, individualAnswer);
 
-        //backtracking
+        // backtracking
         individualAnswer.pop_back();
-
     }
-
-
-
 }
 
-
-int main(){
+int main()
+{
     int index = 0;
-    //dial a number to get all the possible corresponding chars. 
-    string number = "23";
-    //cin>>number;
-    string mapping[10] = {" ", " ", "abc" ,"def" , "ghi" , "jkl" "mno" ,"pqrs" , "tuv" , "wxyz"};
-    vector<string>finalAnswer;
+    // dial a number to get all the possible corresponding chars.
+    string number = "92";
+    // cin>>number;
+    string mapping[10] = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> finalAnswer;
     string individualAns;
-    keypad(index,number,mapping,finalAnswer,individualAns);
-    for(auto element: finalAnswer){
-        cout<<element<<" ";
-        
+    keypad(index, number, mapping, finalAnswer, individualAns);
+    for (auto element : finalAnswer)
+    {
+        cout << element << " ";
     }
-    
-
 }
