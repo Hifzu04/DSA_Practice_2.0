b52c9ba8b6e680b3a5343b2a80371b5283b2adb7
Hifzu04 <hifzurbtech@gmail.com>
2024-05-01 22:31:04 +0530

    init

---
+++ sorting_and_searching/inversionCount.cpp
@@ -0,0 +1,4 @@
+/*Given an array containing integers, u neeed to count the total number of inversions
+Inversion : two element form an inversion, if a[i]< a[j] and i < j
+sample input [0,5,2,3,1]
+sample output 5*/
