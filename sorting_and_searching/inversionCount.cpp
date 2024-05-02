7e4d983700c31bcab035e384c61835f7dae06eff
Hifzu04 <hifzurbtech@gmail.com>
2024-05-02 23:57:18 +0530

    init

--- sorting_and_searching/inversionCount.cpp
+++ sorting_and_searching/inversionCount.cpp
@@ -1,11 +1,16 @@
 /*Given an array containing integers, u neeed to count the total number of inversions
 Inversion : two element form an inversion, if a[i]< a[j] and i < j
 sample input [0,5,2,3,1]
-sample output 5*/
+sample output 5
+
+divide and conquer algorithm(as merge sort)
+ */
 
 #include<iostream>
 #include<vector>
 using namespace std;
 int main (){
-    vector<int>vec
+    vector<int>arr = {0,5,2,3,1};
+
+
 }
