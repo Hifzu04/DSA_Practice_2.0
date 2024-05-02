f70725c75791fa6bfb82f0190481e772bc691148
Hifzu04 <hifzurbtech@gmail.com>
2024-05-02 23:53:15 +0530

    init

--- sorting_and_searching/inversionCount.cpp
+++ sorting_and_searching/inversionCount.cpp
@@ -2,3 +2,10 @@
 Inversion : two element form an inversion, if a[i]< a[j] and i < j
 sample input [0,5,2,3,1]
 sample output 5*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main (){
+    vector<int>vec
+}
