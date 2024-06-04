dce0897f3e2be24e0164d3e4929a2daf32259201
Hifzu04 <hifzurbtech@gmail.com>
2024-06-04 19:44:50 +0530

    few updates

--- sorting_and_searching/inversionCount.cpp
+++ sorting_and_searching/inversionCount.cpp
@@ -4,67 +4,72 @@ sample input [0,5,2,3,1]
 sample output 5
 
 divide and conquer algorithm(as merge sort)
+tc O(Nlogn)
  */
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int merge(vector<int>&array,int s, int e){
-    int i=s;
-    int m = s+(e-s)/2;
-    int j = m+1;
-    vector<int>temp;
-    int cnt = 0; 
-    while(i<=m and j<=e){
-        if(array[i]<array[j]){
+int merge(vector<int> &array, int s, int e)
+{
+    int i = s;
+    int m = s + (e - s) / 2;
+    int j = m + 1;
+    vector<int> temp;
+    int cnt = 0;
+    while (i <= m and j <= e)
+    {
+        if (array[i] < array[j])
+        {
             temp.push_back(array[i]);
             i++;
-        }else{
-            cnt+=(m-i+1);
+        }
+        else
+        {
+            cnt += (m - i + 1);
             temp.push_back(array[j]);
             j++;
         }
-
     }
-    //copy the remaining elementgh
-    while(i<=m){
+    // copy the remaining elementgh
+    while (i <= m)
+    {
         temp.push_back(array[i++]);
+    }
+    while (j <= e)
+    {
+        temp.push_back(array[j++]);
+    }
 
-    }while (j <= e) {
-    temp.push_back(array[j++]);
-}
-
-
-    //copy back the element from temp to original array
-    int k=0;
-    for(int idx= s; idx<= e; idx++){
-        array[idx]= temp[k++];
+    // copy back the element from temp to original array
+    int k = 0;
+    for (int idx = s; idx <= e; idx++)
+    {
+        array[idx] = temp[k++];
     }
     return cnt;
-
 }
 
-int inversion_count(vector<int>&array , int s, int e){
-    //base case 
-    if(s>=e){
+int inversion_count(vector<int> &array, int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
         return 0;
     }
-    int mid = s+(e-s)/2;          //if mid = s+e/2 => shows segementaion fault
+    int mid = s + (e - s) / 2; // if mid = s+e/2 => shows segementaion fault
     int c1 = inversion_count(array, s, mid);
-    int c2 = inversion_count(array,mid+1, e);
+    int c2 = inversion_count(array, mid + 1, e);   
     int ci = merge(array, s, e);
-    return c1+c2+ci;
+    return c1 + c2 + ci;
 }
 
-int main (){
-    vector<int>arr = {0,5,2,3,1};
-    int s = 0 ; 
-    int e = arr.size() -1;
-    cout<<inversion_count(arr, s, e)<<endl;
+int main()
+{
+    vector<int> arr = {0, 5, 2, 3, 1};
+    int s = 0;
+    int e = arr.size() - 1;
+    cout << inversion_count(arr, s, e) << endl;
     return 0;
-
-     
-
-
 }
