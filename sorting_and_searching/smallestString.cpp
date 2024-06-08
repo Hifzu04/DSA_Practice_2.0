0c889e2c221f2e9dc857a47b6beaf6e044c1dc28
Hifzu04 <hifzurbtech@gmail.com>
2024-06-08 15:16:32 +0530

    custom comparator

--- sorting_and_searching/smallestString.cpp
+++ sorting_and_searching/smallestString.cpp
@@ -10,7 +10,7 @@ OUTPUT cbacbc (cba cb c)
 algo : create custom compare function  to comapare and return concatenaton of x before y , or y before x.
 for exmaple : for this array : {"a" , "ab" , "aba"};
 sort(arr , arr+3 )  (without comparetor) => o/p aababa (a ab aba )
-sort(arr , arr+3 , compare) => o/p aabaab (a aba ab ) //lexicographiclly smaller string first
+sort(arr , arr+3 , compare) => o/p aabaab (a aba ab ) //lexicographiclly smaller string first                  
 
 */
 
@@ -20,6 +20,8 @@ sort(arr , arr+3 , compare) => o/p aabaab (a aba ab ) //lexicographiclly smaller
 using namespace std;
  bool comparebro(string x , string y){
      return x+y < y+x ;
+     //if x+y < y+x => return string x ie x will come first in order of sorting.
+     //else => return string y , ie y will come first in order of sorting.
  }
 int main(){
     string arr[] = {"a" , "ab" , "aba"};
@@ -27,6 +29,10 @@ int main(){
     sort(arr, arr+n, comparebro );
 
     for(auto s: arr ){
+        //order of resulting string
         cout<<s<<" "; 
+        cout<<endl;
+        //resulting string
+        cout<<s;
     }
 }
